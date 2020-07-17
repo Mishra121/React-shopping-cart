9213c35575a41c37eb897f75aae4083149fd1fed
Vibhu <vibhu98mishra@gmail.com>
2020-07-17 22:39:28 +0530

    Filtering module implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from "./data.json";
 
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 class App extends React.Component {
   
@@ -15,6 +16,35 @@ class App extends React.Component {
     }
   }
 
+  sortProducts = (event) => {
+    const sort =  event.target.value;
+
+    this.setState((state) => ({
+      sort,
+      products: this.state.products.slice().sort((a, b) => (
+        sort === "lowest" ? 
+          ((a.price > b.price)? 1: -1):
+        sort === "highest" ?
+          ((a.price < b.price)? 1: -1):
+          ((a._id < b._id)? 1: -1)
+      ))
+    }))
+  }
+
+  filterProducts = (event) => {
+    if(event.target.value === "" || event.target.value === "ALL"){
+      this.setState({
+        size: event.target.value,
+        products: data.products
+      })
+    }else{
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+      })  
+    }
+  }
+
   render(){
     return (
       <div className="grid-container">
@@ -24,6 +54,12 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter count={this.state.products.length} 
+                size={this.state.size}
+                sort = {this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products}/>
             </div>
             <div className="sidebar">
