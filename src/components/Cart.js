46791575aa48994e2c2226477b66e72ae6682c53
Vibhu <vibhu98mishra@gmail.com>
2020-08-16 18:17:57 +0530

    Create Order

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import formatCurrency from "../util";
 import Fade from 'react-reveal/Fade'
+import Zoom from 'react-reveal/Zoom'
+import Modal from 'react-modal'
 import { connect } from 'react-redux';
 
 import {removeFromCart} from '../actions/cartActions'
+import { createOrder, clearOrder } from '../actions/orderActions'
 
 class Cart extends Component {
     
@@ -29,13 +32,19 @@ class Cart extends Component {
             email: this.state.email,
             address: this.state.address,
             cartItems: this.props.cartItems,
+            total: this.props.cartItems.reduce((a, c) => a + c.price * c.count, 0)
         };
         this.props.createOrder(order);
+    }
+
+    closeModal = () => {
+        this.props.clearOrder();
+        window.location.reload(false);
     }
     
     render() {
 
-        const {cartItems} = this.props;
+        const {cartItems, order} = this.props;
 
         return (
             <div>
@@ -45,6 +54,62 @@ class Cart extends Component {
                     <div className="cart cart-header">
                         You have {cartItems.length} in the cart{" "}
                     </div>
+                }
+
+                {
+                    order && (
+                        <Modal
+                            isOpen={true}
+                            onRequestClose={this.closeModal}
+                        >
+                        <Zoom>
+                            <button  
+                                onClick={this.closeModal}
+                                className="close-modal"
+                            >
+                                x
+                            </button>
+                            <div className="order-details">
+                                <h3 className="success-message">
+                                    Your order has been placed.
+                                </h3>
+                                <h2>
+                                    Order {order._id}
+                                </h2>
+                                <ul>
+                                    <li>
+                                        <div>Name:</div>
+                                        <div>{order.name}</div>
+                                    </li>
+                                    <li>
+                                        <div>Email:</div>
+                                        <div>{order.email}</div>
+                                    </li>
+                                    <li>
+                                        <div>Address:</div>
+                                        <div>{order.address}</div>
+                                    </li>
+                                    <li>
+                                        <div>Date:</div>
+                                        <div>{order.createdAt}</div>
+                                    </li>
+                                    <li>
+                                        <div>Total:</div>
+                                        <div>{formatCurrency(order.total)}</div>
+                                    </li>
+                                    <li>
+                                        <div>Cart Items:</div>
+                                        <div>{order.cartItems.map((x)=>(
+                                            <div>
+                                                {x.count} {" x "} {x.title}
+                                            </div>
+                                        ))}</div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </Zoom>
+                    </Modal>
+                    )
                 }
 
                 <div>
@@ -144,8 +209,11 @@ class Cart extends Component {
 }
 
 export default connect((state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems
 }),{
-    removeFromCart
+    removeFromCart,
+    createOrder,
+    clearOrder
 })
 (Cart);
--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { productsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducer';
+import { orderReducer } from './reducers/orderReducer'
 
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(combineReducers({
     products: productsReducer,  
-    cart: cartReducer,  
+    cart: cartReducer,
+    order: orderReducer  
 }),
     initialState,
     composeEnhancer(applyMiddleware(thunk))
